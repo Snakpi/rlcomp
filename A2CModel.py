9225a455bb6327d68b2511a84722dd288e22bae6
Vincent Vu <64177421+Snakpi@users.noreply.github.com>
2020-08-05 18:09:57 +0700

    Latest build of an A3C method

--- A2CModel.py
+++ A2CModel.py
@@ -2,93 +2,198 @@
 from warnings import simplefilter 
 simplefilter(action='ignore', category=FutureWarning)
 
-import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn import Sequential
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.autograd import Variable
-import matplotlib.pyplot as plt
-import pandas as pd
+import torch.multiprocessing as mp
+
+from utils import v_wrap, set_init, push_and_pull, record
+from shared_adam import SharedAdam
+
+import math, os, sys
+import numpy as np
+
+from MinerEnv import MinerEnv
+
+###########################################################################################################################
+
+UPDATE_GLOBAL_ITER = 5
+GAMMA = 0.9
+MAX_EP = 10000
+MAX_EP_STEP = 1000
+SAVE_NETWORK = 100  # After this number of episodes, the model is saved for testing later. 
+INPUTNUM = 198 #The number of input values for the model
+ACTIONNUM = 6  #The number of actions output from the model
+MAP_MAX_X = 21 #Width of the Map
+MAP_MAX_Y = 9  #Height of the Map
+
+class Net(nn.Module):
+    def __init__(self, s_dim, a_dim):
+        super(Net, self).__init__()
+        self.s_dim = s_dim
+        self.a_dim = a_dim
+
+        self.al1 = nn.Linear(s_dim, 200)
+        self.al2 = nn.Linear(200, 100)
+        self.al3 = nn.Linear(100, 50)
+        self.al4 = nn.Linear(50, a_dim)
+
+        a_ls = [self.al1, self.al2, self.al3, self.al4]
+
+        self.cl1 = nn.Linear(s_dim, 200)
+        self.cl2 = nn.Linear(200, 100)
+        self.cl3 = nn.Linear(100, 50)
+        self.cl4 = nn.Linear(50, 1)
+
+        cr_ls = [self.cl1, self.cl2, self.cl3, self.cl4]
+
+        set_init(a_ls + cr_ls)
+
+        self.distribution = torch.distributions.Categorical
+
+    def forward(self, x):
+        a1 = F.relu6(self.al1(x))
+        a2 = F.relu6(self.al2(a1))
+        a3 = F.relu6(self.al3(a2))
+        a4 = F.relu6(self.al4(a3))
+        a_probs = F.softmax(a4, dim=1)
+
+        c1 = F.relu6(self.cl1(x))
+        c2 = F.relu6(self.cl2(c1))
+        c3 = F.relu6(self.cl3(c2))
+        v = F.relu(self.cl4(c3))
+
+        return a_probs, v
+
+    def choose_action(self, s, epsilon=.3):
+        self.training = False
+        actions, _ = self.forward(s)
+        sample_0_1 = np.random.sample()
+        if sample_0_1 > epsilon:
+            a = np.argmax(list(a_probs))
+        else:
+            a = np.random.choice(len(list(a_probs)), 1, p=list(a_probs)) #### Semi-greedy epsilon-greedy algorithm (explores the actions by softmax probs)
+        return a
+
+        
+    def loss_func(self, s, a, v_t):
+        self.train()
+        a_probs, v = self.forward(s)
+        td = v_t - v
+        c_loss = td.pow(2)
+
+        m = self.distribution(a_probs)
+        log_prob = m.log_prob(a)
+        exp_v = log_prob * td.detach().squeeze()
+        a_loss = -exp_v
+        total_loss = (a_loss + c_loss).mean()
+        return total_loss
 
 
-# Actor Critic architecture setup
-class ActorCritic(nn.Module): 
-    
-    def __init__(
-            self,
-            num_inputs, #The number of inputs
-            action_space, #The number of actions
-            gamma = 0.99, #The discount factor
-            epsilon = 1, #Epsilon - the exploration factor
-            epsilon_min = 0.01, #The minimum epsilon 
-            epsilon_decay = 0.999,#The decay epislon for each update_epsilon time
-            learning_rate = 0.001, #The learning rate for the DQN network 
-            hidden_sizes = [100, 100, 50],
-            cuda = True
-    ):
-      super(ActorCritic, self).__init__()
-    
-      self.num_inputs = num_inputs
-      self.action_space = action_space
-      self.gamma = gamma
-      self.epsilon = epsilon
-      self.epsilon_min = epsilon_min
-      self.epsilon_decay = epsilon_decay
-      self.learning_rate = learning_rate
-      self.hidden_sizes = hidden_sizes
-            
-      #Creating networks
-      self.critic = self.create_NN(num_inputs, hidden_sizes, 1)
-      self.actor = self.create_NN(num_inputs, hidden_sizes, action_space)
-      self.cuda = cuda
+class Worker(mp.Process):
+    def __init__(self, gnet, opt, global_ep, global_ep_r, res_queue, name, env):
+        super(Worker, self).__init__()
+        self.name = 'w%i' % name
+        self.g_ep, self.g_ep_r, self.res_queue = global_ep, global_ep_r, res_queue
+        self.gnet, self.opt = gnet, opt
+        self.lnet = Net(INPUTNUM, ACTIONNUM)
+        self.env = env
 
-      if self.cuda and torch.cuda.is_available():
-        device = torch.device('cuda')
-      else:
-        device = torch.device('cpu')
-      
-    
-    def create_NN(self, 
-      num_inputs=self.num_inputs, 
-      hidden_sizes=self.hidden_sizes, 
-      num_outputs=self.action_space):
-      '''
-      Creating NN given inputs num, hidden_sizes, and output num
-      (default are those for actor i.e outputs == action_space)
-      '''
-      layers = []
 
-      layers.append(nn.Linear(num_inputs, hidden_sizes[0]))
-      layers.append(nn.ReLU())
+    def run(self):
+        total_step = 1
 
-      for i in range(len(hidden_sizes)):
-        layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
-        layers.append(nn.ReLU())
+        while self.g_ep.value < MAX_EP:
+            try:
+              self.env.reset()
+              s = self.env.get_state()
+              buffer_s, buffer_a, buffer_r = [], [], []
+              ep_r = 0.
+              for t in range(MAX_EP_STEP):
+                  a = self.lnet.choose_action(v_wrap(s[None, :]), .5)
+                  self.env.step(a.clip(-2, 2))
+                  s_ = self.env.get_state()
+                  r = self.env.get_reward()
+                  done = self.env.check_terminate()
 
-      layers.append(nn.Linear(hidden_sizes[-1], num_outputs))
-      layers.append(nn.Softmax())
+                  if t == MAX_EP_STEP - 1:
+                      done = True
+                  ep_r += r
+                  buffer_a.append(a)
+                  buffer_s.append(s)
+                  buffer_r.append(r)
 
-      net = nn.Sequential(*layers)
-      return net
+                  if total_step % UPDATE_GLOBAL_ITER == 0 or done:  # update global and assign to local net
+                      # sync
+                      push_and_pull(self.opt, self.lnet, self.gnet, done, s_, buffer_s, buffer_a, buffer_r, GAMMA)
+                      buffer_s, buffer_a, buffer_r = [], [], []
 
-    
-    def forward(self, state):
-      ''' Forward pass and returns the value & policy dist '''
-      state = Variable(torch.from_numpy(state).float().unsqueeze())
-      value = self.critic(state)
-      policy_dist = self.actor(state)
-      return value, policy_dist
-    
+                      if done:  # done and print information
+                          record(self.g_ep, self.g_ep_r, ep_r, self.res_queue, self.name)
+                          break
+                  s = s_
+                  total_step += 1
 
-    def update_epsilon(self):
-      self.epsilon =  self.epsilon*self.epsilon_decay
-      self.epsilon =  max(self.epsilon_min, self.epsilon)
-      
-      
-    def save_actor(self, path, actor_name):
-        # save model
-        torch.save(self.actor.state_dict(), path + actor_name + '.pth')
-        # serialize weights to HDF5
-        print("Saved actor to disk")
+            except Exception as e:
+              import traceback
+              traceback.print_exc()
+              break
+
+        self.res_queue.put(None)
+
+    def gnet_opt_save(self, ep_r, loss):
+        torch.save(
+            {
+            'global_ep': self.g_ep,
+            'global_ep_r': self.g_ep_r,
+            'gnet state_dict': self.gnet.state_dict(),
+            'opt state_dict': self.opt.state_dict(),
+            }, self.name + '.pth')
+
+########################################################################################################
+
+
+
+# Create header for saving learning file
+'''
+now = datetime.datetime.now() #Getting the latest datetime
+header = ["Ep", "Step", "Reward", "Total_reward", "Action", "Epsilon", "Done", "Termination_Code"] #Defining header for the save file
+filename = "Data/data_" + now.strftime("%Y%m%d-%H%M") + ".csv" 
+with open(filename, 'w') as f:
+    pd.DataFrame(columns=header).to_csv(f, encoding='utf-8', index=False, header=True)
+'''
+
+# Initialize environment
+HOST = "localhost"
+PORT = 1111
+if len(sys.argv) == 3:
+    HOST = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+minerEnv = MinerEnv(HOST, PORT)
+minerEnv.start()
+#train = False #The variable is used to indicate that the epsilon starts to decrease.
+
+#Training Process
+if __name__ == '__main__':
+    gnet = Net(INPUTNUM, ACTIONNUM)        # global network
+    gnet.share_memory()         # share the global parameters in multiprocessing
+    opt = SharedAdam(gnet.parameters(), lr=1e-4, betas=(0.95, 0.999))  # global optimizer
+    global_ep, global_ep_r, res_queue = mp.Value('i', 0), mp.Value('d', 0.), mp.Queue()
+
+    # parallel training
+    workers = [Worker(gnet, opt, global_ep, global_ep_r, res_queue, i, minerEnv) for i in range(mp.cpu_count())]
+    [w.start() for w in workers]
+    res = []
+    while True:
+        r = res_queue.get()
+        if r is not None:
+            res.append(r)
+        else:
+            break
+    [w.join() for w in workers]
+
+    import matplotlib.pyplot as plt
+    plt.plot(res)
+    plt.ylabel('Moving average ep reward')
+    plt.xlabel('Step')
+    plt.show()
